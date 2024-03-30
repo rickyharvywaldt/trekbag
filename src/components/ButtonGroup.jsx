1e26c977e8af439048faa092be880a786937198e
Ricky Waldt <121530629+rickyharvywaldt@users.noreply.github.com>
2024-03-30 13:43:03 +0100

    Create main components

---
+++ src/components/ButtonGroup.jsx
@@ -0,0 +1,12 @@
+import Button from "./Button";
+
+export default function ButtonGroup() {
+  return (
+    <section className="button-group">
+      <Button />
+      <Button />
+      <Button />
+      <Button />
+    </section>
+  );
+}
---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+    <footer>
+      <small>&copy; 2024. All rights reserved.</small>
+      <small>
+        Version <b>1.5</b>
+      </small>
+    </footer>
+  );
+}
